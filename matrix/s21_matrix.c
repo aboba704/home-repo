08409e82db295c49bae544fa45238c6c5ef49bda
lunchlma <lunchlma@student-nsk.21-school.ru>
2025-04-01 20:47:44 +0700

    matrix

---
+++ matrix/s21_matrix.c
@@ -0,0 +1,27 @@
+#include "s21_matrix.h"
+
+int s21_create_matrix(int rows, int columns, matrix_t *result) {
+  if (rows < 1 || columns < 1) return 1;
+  result->matrix = (double **)calloc(rows, sizeof(double *));
+  if (result->matrix == NULL) return 1;
+  result->rows = rows;
+  result->columns = columns;
+  for (int i = 0; i < rows; ++i) {
+    result->matrix[i] = (double *)calloc(columns, sizeof(double));
+    if (result->matrix[i] == NULL) {
+      for (int j = 0; j < i; ++j) free(result->matrix[j]);
+      free(result->matrix);
+    }
+  }
+
+  return 0;
+}
+
+void s21_remove_matrix(matrix_t *A) {
+  if (A->matrix != NULL) {
+    for (int i = 0; i < A->rows; ++i) free(A->matrix[i]);
+    free(A->matrix);
+  }
+  A->rows = 0;
+  A->columns = 0;
+}
---
+++ matrix/tests/test_matrix.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <check.h>
+#include <limits.h>
+#include <time.h>
+
+#include "../s21_matrix.h"
+
+Suite *test_create_matrix();
+// Suite *test_remove_matrix();
+// Suite *test_eq_matrix();
+// Suite *test_sum_matrix();
+// Suite *test_sub_matrix();
+// Suite *test_mult_number();
+// Suite *test_mult_matrix();
+// Suite *test_transpose();
+
+// Suite *test_calc_complements();
+// Suite *test_determinant();
+// Suite *test_inverse_matrix();
+
+double get_rand(double min, double max);
